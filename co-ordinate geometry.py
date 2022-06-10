76a38bcd9f5464c2bf20be7a1b6872b3bf27b6b1
Soumyajit01 <soumyajitdass06@gmail.com>
2022-06-10 19:24:12 +0530

    modified the area() function

--- co-ordinate geometry.py
+++ co-ordinate geometry.py
@@ -108,7 +108,7 @@ def areaOfTriangle(pt1, pt2, pt3):
     x1, y1 = pt1[0], pt1[1]
     x2, y2 = pt2[0], pt2[1]
     x3, y3 = pt3[0], pt3[1]
-    area = 1/2*(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2))
+    area = 1/2* abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2))
     print(f"Area of triangle is {area} sq. units")
 
 areaOfTriangle((2, -2), (4, 2), (-1, 3))
